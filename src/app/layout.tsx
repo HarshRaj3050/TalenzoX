96dca8724890aefca80bd48b8b27070c6210c9eb
HarshRaj3050 <harshraj3060@gmail.com>
2026-09-13 00:03:24 +0530

    improve: update site URL configuration to use environment variable and adjust default theme in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import DarkIcon from "next/dist/next-devtools/dev-overlay/icons/dark-icon";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +30,8 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          themes={["dark"]}
+          defaultTheme="light"
+          themes={["light"]}
           enableSystem={false}
         >
           {children}
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "TalenzoX",
   description: "The best platform for learning",
-  url: "http://localhost:3000",
+  url: process.env.NEXT_PUBLIC_TALENZOX_URL || "http://localhost:3000",
   supportEmail: "harshai20042006@gmail.com",
 } as const;
